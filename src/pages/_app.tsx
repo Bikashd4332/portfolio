95d4b8363850875695fa588fa98f36f47170332f
Bikash Das <bikash.das@glxdigital.com>
2023-02-12 02:56:47 +0800

    Adds fonts and Link component style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,40 @@
+import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import localFont from '@next/font/local';
 
 import theme from '@/theme';
-import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const sfMono = localFont({
+    src: [
+        {
+            path: '../../public/fonts/SFMonoLight.woff',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/SFMonoLightItalic.woff',
+            weight: '400',
+            style: 'italic',
+        },
+        {
+            path: '../../public/fonts/SFMonoBold.woff',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: '../../public/fonts/SFMonoBoldItalic.woff',
+            weight: '700',
+            style: 'italic',
+        },
+    ],
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+        <ChakraProvider theme={theme} resetCSS>
+            <main className={sfMono.className}>
+                <Component {...pageProps} />
+            </main>
         </ChakraProvider>
     );
 }
-
-export default MyApp;
