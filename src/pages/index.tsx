f3fcc827f62a6f97f8012f9122c3f99f36509665
Bikash Das <bikash.das@glxdigital.com>
2023-02-15 01:03:34 +0800

    Adding og meat tags and refactoring.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,12 @@
-import Head from 'next/head';
-
 import { HeroSection } from '@/containers/HeroSection/HeroSection';
+import { MetaInfo } from '@/containers/MetaInfo';
 import { Container } from '@/components/container';
 import { NavBar } from '@/components/NavBar';
 
 export default function Index() {
     return (
         <>
-            <Head>
-                <title>Bikash Das</title>
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width, shrink-to-fit=no"
-                />
-                <meta name="description" content="" />
-                <meta
-                    name="description"
-                    content="Bikash Das is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences."
-                    data-react-helmet="true"
-                ></meta>
-            </Head>
+            <MetaInfo />
             <NavBar />
             <Container>
                 <HeroSection />
