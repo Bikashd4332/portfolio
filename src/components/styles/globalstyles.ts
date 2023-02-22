0aa042d7ee391a825eba32b247df2351ed0cbff5
Bikash Das <bikash.das@glxdigital.com>
2023-02-22 22:55:35 +0800

    fix(respnsive) making more responsive

--- src/components/styles/globalstyles.ts
+++ src/components/styles/globalstyles.ts
@@ -2,8 +2,13 @@ const globalStyles = {
     global: () => ({
         body: {
             bg: 'navyblue.600',
+            width: '100%',
+            minHeight: '100%',
+            overflowX: 'hidden',
+            '-webkit-font-smoothing': 'antialiased',
             fontSize: { base: 'lg', md: 'xl' },
             lineHeight: '1.3',
+            color: 'slate.700',
         },
         p: {
             color: 'slate.700',
@@ -12,7 +17,6 @@ const globalStyles = {
         'h1, h2, h3, h4, h5, h6': {
             margin: '0px 0px 10px',
             fontWeight: '700',
-            color: 'slate.700',
         },
         'ol,ul': {
             listStyleType: 'none',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import { HeroSection } from '@/containers/HeroSection/HeroSection';
-import { MetaInfo } from '@/containers/MetaInfo';
 import { Container } from '@/components/container';
 import { NavBar } from '@/components/NavBar';
 
 export default function Index() {
     return (
         <>
-            <MetaInfo />
             <NavBar />
             <Container as="main">
                 <HeroSection />
