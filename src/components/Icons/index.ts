d1b988c8d0fb6d803acf51b17ad29e98df0d8ddc
Bikash Das <bikash.das@glxdigital.com>
2023-02-24 03:06:40 +0800

    feat(social) Addes social links

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,5 @@
+export { default as CodePen } from './codepen';
+export { default as Instagram } from './instagram';
+export { default as Twitter } from './twitter';
+export { default as LinkedIn } from './linkedin';
+export { default as Github } from './github';
