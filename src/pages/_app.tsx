46061750b632413da0eaf278c0039a04aebd6601
Bikash Das <bikash.das@classicinformatics.com>
2024-11-20 18:22:53 +0530

    added speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,53 +6,53 @@ import theme from '@/theme';
 import { SWRProvider } from '@/providers/SWRProvider';
 
 const sfMono = localFont({
-    src: [
-        {
-            path: '../../public/fonts/SFMonoLight.woff',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '../../public/fonts/SFMonoLightItalic.woff',
-            weight: '400',
-            style: 'italic',
-        },
-        {
-            path: '../../public/fonts/SFMonoBold.woff',
-            weight: '700',
-            style: 'normal',
-        },
-        {
-            path: '../../public/fonts/SFMonoBoldItalic.woff',
-            weight: '700',
-            style: 'italic',
-        },
-    ],
+  src: [
+    {
+      path: '../../public/fonts/SFMonoLight.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SFMonoLightItalic.woff',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/SFMonoBold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/SFMonoBoldItalic.woff',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
 });
 
 const calibreFont = localFont({
-    src: [
-        {
-            path: '../../public/fonts/Calibre-Regular.woff2',
-            weight: '400',
-            style: 'normal',
-        },
-        {
-            path: '../../public/fonts/Calibre-Semibold.woff2',
-            weight: '700',
-            style: 'italic',
-        },
-    ],
+  src: [
+    {
+      path: '../../public/fonts/Calibre-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Calibre-Semibold.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
 });
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <ChakraBaseProvider theme={theme} resetCSS>
-            <SWRProvider>
-                <div className={`${sfMono.className} ${calibreFont.className}`}>
-                    <Component {...pageProps} />
-                </div>
-            </SWRProvider>
-        </ChakraBaseProvider>
-    );
+  return (
+    <ChakraBaseProvider theme={theme} resetCSS>
+      <SWRProvider>
+        <div className={`${sfMono.className} ${calibreFont.className}`}>
+          <Component {...pageProps} />
+        </div>
+      </SWRProvider>
+    </ChakraBaseProvider>
+  );
 }
